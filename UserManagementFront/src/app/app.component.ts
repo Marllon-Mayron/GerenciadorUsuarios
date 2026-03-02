5a175b6aae52aab0c8dbeb1dd4c5a1ffd3366bce
Marllon Silva <marllon.silva@mv.com.br>
2026-03-01 22:44:05 -0300

    Adicionado conteudos no footer do site

--- UserManagementFront/src/app/app.component.ts
+++ UserManagementFront/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
 import { ToastComponent } from './shared/components/toast/toast.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HomeComponent, ToastComponent,],
+  imports: [CommonModule, RouterOutlet, HomeComponent, ToastComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
